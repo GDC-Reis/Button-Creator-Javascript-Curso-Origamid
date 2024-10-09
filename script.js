98b3c61866bc7dc8270feac4c6bc3471a9100cc2
GDC-Reis <gustavodonisete11@gmail.com>
2024-10-08 21:49:20 -0300

    Commit realizado com adição do localStorage

--- script.js
+++ script.js
@@ -33,16 +33,31 @@ const handleStyle = {
     fontSize(value) {
       this.element.style.fontSize = value + 'rem';
     },
-  }
+}
 
 function handleChange(event){
     const name = event.target.name;
     const value = event.target.value;
 
     handleStyle[name](value);
+    saveValues(name, value);
     showCss();
 }
 
+function saveValues(name, value){
+  localStorage[name] = value
+}
+
+function setValues() {
+  const properties = Object.keys(localStorage);
+  properties.forEach(propertie => {
+    handleStyle[propertie](localStorage[propertie]);
+    controles.elements[propertie].value = localStorage[propertie];
+  })
+  showCss();
+}
+
+setValues();
 
 function showCss(){
     cssText.innerHTML = '<span>' + btn.style.cssText.split('; ').join(';</span><span>');
